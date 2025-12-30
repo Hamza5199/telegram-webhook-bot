20bfc9563c76d8084036294ca7bb197d3561813f
Hamza5199 <85038852+Hamza5199@users.noreply.github.com>
2025-12-31 01:16:00 +0500

    fetch btc price

--- scalping_bot.py
+++ scalping_bot.py
@@ -1,1 +1,15 @@
-print("Scalping bot started")
+import ccxt
+
+exchange = ccxt.binance({
+    "enableRateLimit": True,
+    "options": {
+        "defaultType": "future"
+    }
+})
+
+def get_price():
+    ticker = exchange.fetch_ticker("BTC/USDT")
+    price = ticker["last"]
+    print("BTC price:", price)
+
+get_price()
